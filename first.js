ed6b7c28b6532232c9fb3b560b67853f7b154391
Kushak Jafry <kushakjafry@gmail.com>
2024-04-20 17:23:50 +0530

    first js file added

---
+++ first.js
@@ -0,0 +1,42 @@
+// Variable - let, const, var
+
+// let can be changed
+let no = 1.2;
+let str = "adcas";
+let chr = "a";
+let arr = [1, 2, 3, 4, 5];
+console.log(typeof no);
+console.log("Hello");
+
+// const cannoted be changed/assigned
+const noC = 2;
+const strC = "adsasd";
+const chrC = "v";
+const arrC = [1, 2, 3, 4, 5];
+// noC = 2; Cannot be assigned
+
+var noV = 1;
+var strV = "asdasd";
+
+let name = "Name";
+
+let x = 2;
+let y = 5;
+
+let z = x + y; // addition
+console.log(z);
+
+let a = "Hello";
+let b = "World";
+
+let c = a + " " + b; // concatenation
+console.log(c);
+
+let p = 2;
+let q = "23";
+let r = p + q;
+let s = q + p;
+let mu = p * q;
+console.log(r);
+console.log(s);
+console.log(mu);
